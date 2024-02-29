1b188fed663db09a392051df2e46856798a7dfc2
M.Srikanth Karthikeyan <143238328+MSrikanthkarthikeyan@users.noreply.github.com>
2024-02-29 21:49:52 +0530

    Create fib.c

---
+++ fib.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int fib(int n);
+
+int main(){
+    int n;
+    printf("Enter the number n:");
+    scanf("%d",&n);
+    printf("The fibonacci of %d is %d",n,fib(n));
+    return 0;
+}
+
+int fib(int n){
+    if(n==0){
+        return 0;
+    }
+    else if(n==1){
+        return 1;
+    }
+    else{
+    return fib(n-1)+fib(n-2);
+    }
+}
